8aa0a5e6d63dfc81d1fd0a973fc7312360494bac
shenshuo <s_sure@hotmail.com>
2020-04-17 16:13:33 +0800

    adding connection pool for tornado-mysql

--- db/__init__.py
+++ db/__init__.py
@@ -9,7 +9,7 @@ pools.DEBUG = True
 
 
 POOL = pools.Pool(
-    dict(host='127.0.0.1', port=3306, user='root', passwd='', db='mysql'),
+    dict(host='127.0.0.1', port=3306, user='root', passwd='19901101', db='mysql'),
     max_idle_connections=2,
     max_recycle_sec=3,
     max_open_connections=5,

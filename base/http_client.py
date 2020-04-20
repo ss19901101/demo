5da040384121c64929f1a8d7341e4d00a919fd63
shenshuo <s_sure@hotmail.com>
2020-04-20 09:49:58 +0800

    add http client method using AsyncHTTPClient

---
+++ base/http_client.py
@@ -0,0 +1,41 @@
+from tornado.httpclient import AsyncHTTPClient, HTTPRequest
+
+BASE_PREFIX = 'http://localhost:8081/'
+
+
+def get(url='', params=None):
+    """
+
+    :type url: str
+    :type params: object
+    """
+    if params is None:
+        params = {}
+    query_str = '?'
+    for key in params:
+        query_str += key + '=' + params[key] + '&'
+    url = url + query_str[0:-1]
+    return http(url=url)
+
+
+def post(url='', body=''):
+    """
+
+    :param url:
+    :param body:
+    :return: HTTPResponse
+    """
+    return http(url=url, method='POST', body=body)
+
+
+async def http(url='', method='GET', body=''):
+    """
+
+    :param url:
+    :param method:
+    :param body:
+    :return: HTTPResponse
+    """
+    http_client = AsyncHTTPClient()
+    request = HTTPRequest(url=BASE_PREFIX + url, method=method, body=body)
+    return await http_client.fetch(request)

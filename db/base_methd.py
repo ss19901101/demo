7171604385deb076a5939d0a1421bfcdd7ee386d
shenshuo <s_sure@hotmail.com>
2020-04-20 15:39:03 +0800

    update add-method change return file to rowid add update-method for updating database

--- db/base_methd.py
+++ db/base_methd.py
@@ -15,28 +15,54 @@ def query(query_str):
 
 @gen.coroutine
 def add(obj):
+    """
+
+    :param obj: object
+    :return obj: object
+
+    this method is a simple orm method for saving a object to the database
+    and returns the object with an auto-generated id
+    """
     fields = []
     params = []
     args = []
     for attr in dir(obj):
         if attr in obj.attribute_map:
             arg_temp = getattr(obj, attr)
-            if (not isinstance(arg_temp, list) \
-                    or not isinstance(arg_temp, object)) \
+            if (not isinstance(arg_temp, list) or not isinstance(arg_temp, object)) \
                     and arg_temp is not None:
                 fields.append(attr)
                 params.append('%s')
                 args.append(arg_temp)
     sql = 'insert into %s (%s) values (%s)' % (
         obj.__class__.__name__.lower(), ','.join(fields), ','.join(params))
-    print(sql)
-    print(args)
-    temp = yield POOL.execute(sql, args)
-    return temp
+    cursor = execute_sql(sql, args)
+    obj_id = cursor.lastrowid
+    return obj_id
 
+
+@gen.coroutine
+def execute_sql(sql, args):
+    cursor = yield POOL.execute(sql, args)
+    return cursor
+
+@gen.coroutine
 def delete(str):
     pass
 
 
-def update(str):
-    pass
+@gen.coroutine
+def update(obj):
+    fields = []
+    args = []
+    for attr in dir(obj):
+        if attr in obj.attribute_map:
+            arg_temp = getattr(obj, attr)
+            if (not isinstance(arg_temp, list) or not isinstance(arg_temp, object)) \
+                    and arg_temp is not None and attr is not 'id':
+                attr = str(attr) + '= %s'
+                fields.append(attr)
+                args.append(arg_temp)
+    sql = 'update %s set %s where id = %s' % (
+        obj.__class__.__name__.lower(), ','.join(fields), obj.id)
+    yield POOL.execute(sql, args)

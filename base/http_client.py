68482347827736071f443e16b4b2e4c8a8a095ff
shenshuo <s_sure@hotmail.com>
2020-04-21 18:29:29 +0800

    update HTTPClient from get data from both controller and api

--- base/http_client.py
+++ base/http_client.py
@@ -3,39 +3,59 @@ from tornado.httpclient import AsyncHTTPClient, HTTPRequest
 BASE_PREFIX = 'http://localhost:8081/'
 
 
-def get(url='', params=None):
-    """
+class HTTPClient:
+    def __init__(self, base_prefix):
+        """
 
-    :type url: str
-    :type params: object
-    """
-    if params is None:
-        params = {}
-    query_str = '?'
-    for key in params:
-        query_str += key + '=' + str(params[key]) + '&'
-    url = url + query_str[0:-1]
-    return http(url=url)
+        :type base_prefix: str
+        """
+        self._base_prefix = None
+        if base_prefix is not None:
+            self._base_prefix = base_prefix
+
+    @property
+    def base_prefix(self):
+        return self._base_prefix
+
+    @base_prefix.setter
+    def base_prefix(self, base_prefix):
+        self._base_prefix = base_prefix
+
+    def get(self, url='', params=None):
+        """
+
+        :type url: str
+        :type params: object
+        """
+        if params is None:
+            params = {}
+        query_str = '?'
+        for key in params:
+            query_str += key + '=' + str(params[key]) + '&'
+        url = url + query_str[0:-1]
+        return self.http(url=url)
+
+    def post(self, url='', body=''):
+        """
+
+        :param url:
+        :param body:
+        :return: HTTPResponse
+        """
+        return self.http(url=url, method='POST', body=body)
+
+    async def http(self, url='', method='GET', body=None):
+        """
+
+        :param url:
+        :param method:
+        :param body:
+        :return: HTTPResponse
+        """
+        http_client = AsyncHTTPClient()
+        request = HTTPRequest(url=BASE_PREFIX + url, method=method, body=body)
+        return await http_client.fetch(request)
 
 
-def post(url='', body=''):
-    """
-
-    :param url:
-    :param body:
-    :return: HTTPResponse
-    """
-    return http(url=url, method='POST', body=body)
-
-
-async def http(url='', method='GET', body=None):
-    """
-
-    :param url:
-    :param method:
-    :param body:
-    :return: HTTPResponse
-    """
-    http_client = AsyncHTTPClient()
-    request = HTTPRequest(url=BASE_PREFIX + url, method=method, body=body)
-    return await http_client.fetch(request)
+client_controller = HTTPClient('http://localhost:8081/')
+client_api = HTTPClient('http://localhost:8080/')
